43aae4f3e4c685e7a693a94f27bda5261d07dd3f
Your Name <you@example.com>
2025-05-01 01:43:06 +0200

    feat: release prod

--- init-env.sh
+++ init-env.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+if [ -d ".terraform" ]; then
+  sudo rm -r .terraform
+fi
+
 branch=$(git rev-parse --abbrev-ref HEAD)
-
-
 
 cp ./providers/providers.$branch.tf providers.tf
 cp ./tfvars/terraform.$branch.tfvars terraform.tfvars
